d641c191914e127977049142aca89ea20d68c7cf
Stanislav Malyshev <smalyshev@gmail.com>
2018-07-17 17:24:31 -0700

    Exit when done

--- index.php
+++ index.php
@@ -5,6 +5,7 @@ if ( !empty( $_GET['update'] ) ) {
 	$tabulist = new Tabulist( 'commonswiki' );
 	$tabulist->setVerbose( true );
 	$tabulist->updatePage( $tabulist->getPageByTitle( $_GET['update'] ) );
+	exit(0);
 }
 
 echo file_get_contents( __DIR__ . '/index.html' );
